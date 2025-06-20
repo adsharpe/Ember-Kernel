243dccce161b6e45c0ca68d7e7db6380133b7d3c
Apollo D. Sharpe, Sr. <1580939+adsharpe@users.noreply.github.com>
2025-06-20 15:41:23 -0500

    Kernel prints memory misaligned version of string to the screen.

--- src/kmain.cpp
+++ src/kmain.cpp
@@ -1,20 +1,16 @@
-extern "C" {
 
 
-
-int kmain()
+extern "C" int kmain()
 {
 	const unsigned int VID_MEM_START = 0xB8000;
-	unsigned char *pScreen = (unsigned char *)VID_MEM_START;
+	volatile unsigned char *pScreen = (volatile unsigned char *)VID_MEM_START;
 	char nColor = 0xF0;
 	char *pMsg = (char *)"Hello from the Ember Kernel!!!";
 
-	while(pMsg != 0) {
-		*pScreen++ = *pMsg++;
-		*pScreen++ = nColor;
+	for(int nIndex = 0; pMsg[nIndex] != '\0'; nIndex++) {
+        pScreen[nIndex * 2]     = pMsg[nIndex];
+        pScreen[nIndex * 2 + 1] = nColor;
 	}
 
 	return 0;
-}
-
 }
